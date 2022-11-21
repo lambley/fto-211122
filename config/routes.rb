debc7124488996116dac46f0b4615b1431e37387
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-21 16:49:36 +0000

    fix direct_post method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 's3/direct_post'
+  post 's3/direct_post'
   get 's3/get_images'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
