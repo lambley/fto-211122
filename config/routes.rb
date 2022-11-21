b3070f070e22e7ac0499fd7db6e547df0d4bebf7
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-21 15:46:11 +0000

    add get_images route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 's3/direct_post'
+  get 's3/get_images'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
