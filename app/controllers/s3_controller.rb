bd5720990a85c91490b9a4f57688201f3bed5961
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-21 16:47:38 +0000

    update direct_post method - still WIP

--- app/controllers/s3_controller.rb
+++ app/controllers/s3_controller.rb
@@ -1,8 +1,10 @@
-require "aws-sdk-s3"
+require 'aws-sdk-s3'
 
 class S3Controller < ApplicationController
+  # this method would be for posting images directly to s3 bucketn - still WIP
   def direct_post
-    data = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201',
+    filename = params[:myImage]
+    data = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/#{filename}", success_action_status: '201',
                                     acl: 'public-read')
 
     render json: { url: data.url, fields: data.fields }, status: :ok
@@ -11,7 +13,7 @@ class S3Controller < ApplicationController
   def get_images
     s3 = Aws::S3::Client.new
     keys = []
-    s3.list_objects(bucket: "fto-211122").each do |response|
+    s3.list_objects(bucket: 'fto-211122').each do |response|
       keys << response.contents.map(&:key)
     end
 
