5cec3fc292217eb317269cf599ad1920b2df736d
Aaron Lambley <aaron.lambley@gmail.com>
2022-11-21 15:22:03 +0000

    create get images - for object keys

--- app/controllers/s3_controller.rb
+++ app/controllers/s3_controller.rb
@@ -1,8 +1,20 @@
+require "aws-sdk-s3"
+
 class S3Controller < ApplicationController
   def direct_post
     data = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201',
                                     acl: 'public-read')
 
     render json: { url: data.url, fields: data.fields }, status: :ok
+  end
+
+  def get_images
+    s3 = Aws::S3::Client.new
+    keys = []
+    s3.list_objects(bucket: "fto-211122").each do |response|
+      keys << response.contents.map(&:key)
+    end
+
+    render json: { keys: }
   end
 end
